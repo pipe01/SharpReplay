0a4d5bf46bf948b7655434807c2ec598eff83b75
Felipe Martinez <pipernene@gmail.com>
2019-03-15 20:06:55 +0100

    Upload

--- SharpReplay/MainWindow.xaml.cs
+++ SharpReplay/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ScreenRecorderLib;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,60 @@ namespace SharpReplay
         public MainWindow()
         {
             InitializeComponent();
+
+            var cont = new ContinuousStream(3, 3);
+            var data = Enumerable.Range(0, 50).Select(o => (byte)(o + 1)).ToArray();
+
+            cont.Write(data, 0, data.Length);
+
+            var mem = new MemoryStream();
+            cont.CopyTo(mem);
+
+            var a = mem.ToArray();
+        }
+
+        private Recorder Rec;
+        private ContinuousStream RecStream;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("out.mp4"))
+                File.Delete("out.mp4");
+
+            RecStream?.Dispose();
+            RecStream = new ContinuousStream(100, 1 * 1024 * 1024);
+
+            Rec = Recorder.CreateRecorder(new RecorderOptions
+            {
+                VideoOptions = new VideoOptions
+                {
+                    BitrateMode = BitrateControlMode.Quality,
+                    Bitrate = 8000 * 1000,
+                    Framerate = 60,
+                    Quality = 70,
+                    IsMousePointerEnabled = false,
+                    IsFixedFramerate = true,
+                    EncoderProfile = H264Profile.Main
+                },
+                AudioOptions = new AudioOptions
+                {
+                    Bitrate = AudioBitrate.bitrate_128kbps,
+                    Channels = AudioChannels.Stereo,
+                    IsAudioEnabled = true
+                },
+                IsLowLatencyEnabled = true,
+                IsFragmentedMp4Enabled = true,
+                //IsMp4FastStartEnabled = true,
+                RecorderMode = RecorderMode.Video
+            });
+            Rec.Record(RecStream);
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Rec.Stop();
+
+            RecStream.CopyTo(File.OpenWrite("out.mp4"));
         }
     }
 }
