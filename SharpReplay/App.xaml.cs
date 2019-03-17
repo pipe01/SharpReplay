14f60dfe7a60a49bc15bc721ad081eb549e1f17d
Felipe Martinez <pipernene@gmail.com>
2019-03-17 14:03:34 +0100

    Add unhandled exception handling

--- SharpReplay/App.xaml.cs
+++ SharpReplay/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Anotar.Log4Net;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -14,6 +15,20 @@ namespace SharpReplay
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+
+            LogTo.FatalException("Unhandled exception", exception);
+
+            MessageBox.Show($"{exception.GetType().FullName}: {exception.Message}\n", "Unhandled exception", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
